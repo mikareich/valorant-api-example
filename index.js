ee70151adb9062a85f97b1140fa6df60ac25c411
Mika Reich <mikareich0@gmail.com>
2023-11-22 23:33:59 +0100

    uwu

--- index.js
+++ index.js
@@ -31,4 +31,15 @@ async function fetchStore(username, password, region) {
   };
 }
 
-fetchStore("rikameich", "a9t13zy44gfd", "eu").then(console.log);
+// region = eu, na, ap...
+fetchStore("username", "password", "region").then(console.log);
+
+// {
+//   store: [
+//     { id: '87220abc-49a9-7472-ae09-29a6d711bbc3', price: 2175 },
+//     { id: '7302d465-46cd-2a43-655f-93aee59030db', price: 3550 },
+//     { id: '3bf9d39f-46b4-ba23-e079-488d799b9416', price: 1275 },
+//     { id: '0296ff9b-4172-5a29-7d25-a7bf491b5bd4', price: 1775 }
+//   ],
+//   resetsAt: 2023-11-22T23:59:58.709Z
+// }
